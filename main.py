11bb03e6a6c38553309d05d2130e3797e32c0197
Dmitry Romanov <dev@wutiarn.com>
2017-09-24 19:32:07 +0300

    WIP: Telepot setup

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-from aiohttp import web
+import asyncio
 import logging
 import os
 
+import telepot
+from aiohttp import web
 from aiohttp.web_exceptions import HTTPNotFound
-
-TOKEN = os.environ["sched.telegram.tg_token"]
+from telepot.aio import Bot
+from telepot.aio.loop import Webhook, MessageLoop
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("main")
+
+TOKEN = os.environ["sched.telegram.tg_token"]
+
+
+def on_chat_message(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    print('Chat Message:', content_type, chat_type, chat_id)
+
+
+bot = Bot(TOKEN)
+webhook = Webhook(bot, {
+    'chat': on_chat_message
+})
+
+# loop = asyncio.get_event_loop()
+# loop.create_task(MessageLoop(bot).run_forever())
 
 
 async def handle(request):
@@ -18,6 +36,7 @@ async def handle(request):
 
     data = await request.json()
     logger.info(f"Received telegram webhook: {data}")
+    webhook.feed(data)
     return web.Response(text="OK")
 
 
